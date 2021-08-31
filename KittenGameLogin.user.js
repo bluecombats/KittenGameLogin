adb94b26e36bfeb5f942fd1857b67cc968b5cc33
James Clare <bluecombats@users.noreply.github.com>
2021-08-31 15:51:12 +0100

    Create KittenGameLogin.user.js

---
+++ KittenGameLogin.user.js
@@ -0,0 +1,29 @@
+/ ==UserScript==
+// @name        KGLogin
+// @namespace   https://github.com/bluecombats/KittenGameLogin/edit/main/dist/KittenGameLogin.user.js
+// @description progress bars
+// @include     *kittensgame.com/web/*
+// @include     *kittensgame.com/beta/*
+// @include     *kittensgame.com/alpha/*
+// @version     1.10003
+// @grant       none
+// @run-at 	document-end
+// ==/UserScript==
+
+function initKGL(){
+	KGPInterval = setInterval(function(){
+		InsertLogin();
+	}, 1000);
+}
+
+function InsertLogin(){
+  if(document.getElementsByClassName("login-popup button_tooltip tooltip-block")[0]){
+    var login = document.getElementsByClassName("login-popup button_tooltip tooltip-block")[0];
+    //console.log(login);
+    //https://developer.mozilla.org/en-US/docs/Web/HTML/Element/input/password
+    login.getElementsByTagName("input")[0].setAttribute("automComplete","on");
+    login.getElementsByTagName("input")[1].setAttribute("automComplete","on");
+  }
+}
+//Main script starts here
+initKGL();
