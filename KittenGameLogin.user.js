920c1634f82dc0809057d2cda8f5b6bdf8c29afc
James Clare <bluecombats@users.noreply.github.com>
2021-09-08 09:54:44 +0100

    fix bugs
    fixed a bug or 2 and put in an extra if clause to prevent errors from occuring

--- KittenGameLogin.user.js
+++ KittenGameLogin.user.js
@@ -1,17 +1,17 @@
-/ ==UserScript==
+// ==UserScript==
 // @name        KGLogin
-// @namespace   https://github.com/bluecombats/KittenGameLogin/edit/main/dist/KittenGameLogin.user.js
+// @namespace   https://github.com/bluecombats/KittenGameLogin/raw/main/KittenGameLogin.user.js
 // @description progress bars
 // @include     *kittensgame.com/web/*
 // @include     *kittensgame.com/beta/*
 // @include     *kittensgame.com/alpha/*
-// @version     1.10003
+// @version     1.10004
 // @grant       none
 // @run-at 	document-end
 // ==/UserScript==
 
 function initKGL(){
-	KGPInterval = setInterval(function(){
+	var KGPInterval = setInterval(function(){
 		InsertLogin();
 	}, 1000);
 }
@@ -21,8 +21,10 @@ function InsertLogin(){
     var login = document.getElementsByClassName("login-popup button_tooltip tooltip-block")[0];
     //console.log(login);
     //https://developer.mozilla.org/en-US/docs/Web/HTML/Element/input/password
-    login.getElementsByTagName("input")[0].setAttribute("automComplete","on");
-    login.getElementsByTagName("input")[1].setAttribute("automComplete","on");
+    if(login.getElementsByTagName("input")[0]){
+       login.getElementsByTagName("input")[0].setAttribute("automComplete","on");
+       login.getElementsByTagName("input")[1].setAttribute("automComplete","on");
+    }
   }
 }
 //Main script starts here
